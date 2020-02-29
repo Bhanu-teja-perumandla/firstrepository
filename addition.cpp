1088288f9ab6146fba95a3e84d7b7a9ffd5dd7d9
Bhanu-teja-perumandla <perumandlabhanuteja@gmail.com>
2020-02-29 15:23:33 +0530

    addition.cpp file is added

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+#inlude<iostream>
+using namespace std;
+
+int main(){
+	int a,b,res;
+	cout<<"Enter two numbers to add\n";
+	cin>>a>>b;
+	res=a+b;
+	cout<<"sum of a and b = "<<res<<endl;
+	
+	
+	return 0;
+}
