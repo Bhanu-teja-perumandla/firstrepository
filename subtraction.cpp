844fedc834201a5b8b392afd96800a1ba1fe7cc1
Bhanu-teja-perumandla <perumandlabhanuteja@gmail.com>
2020-02-29 15:33:52 +0530

    subtraction.cpp file is added

---
+++ subtraction.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,res;
+	
+	cout<<"Enter two numbers"<<endl;
+	cin>>a>>b;
+	
+	res=a-b;
+	cout<<"difference of two numbers = "<<res;
+	
+	return 0;
+}
